fa3d1344567a3602b9f1b45c1ccc8f5748f5c1f2
betongame-web <asimhalder612@gmail.com>
2026-04-20 15:51:03 +0600

    Update FortunetigerController.php

--- app/Http/Controllers/Games/FortunetigerController.php
+++ app/Http/Controllers/Games/FortunetigerController.php
@@ -156,4 +156,21 @@ class FortunetigerController extends Controller
     {
         return FortuneTigerIcons::getIcons();
     }
+public function change_free(Request $request, $token = null)
+{
+    return response()->json([
+        'success' => true,
+        'message' => 'Change free success',
+        'data' => [],
+    ], 200);
+}
+
+public function history_detail(Request $request, $token = null)
+{
+    return response()->json([
+        'success' => true,
+        'message' => 'History detail success',
+        'data' => [],
+    ], 200);
+}
 }
