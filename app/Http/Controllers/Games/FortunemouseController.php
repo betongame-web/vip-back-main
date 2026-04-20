208b84ddbe039c99f9e28b674a8dcb394f0225b3
betongame-web <asimhalder612@gmail.com>
2026-04-20 15:49:04 +0600

    Update FortunemouseController.php

--- app/Http/Controllers/Games/FortunemouseController.php
+++ app/Http/Controllers/Games/FortunemouseController.php
@@ -147,4 +147,21 @@ class FortunemouseController extends Controller
     {
         return FortuneMouseIcons::getIcons();
     }
+public function change_free(Request $request, $token = null)
+{
+    return response()->json([
+        'success' => true,
+        'message' => 'Change free success',
+        'data' => [],
+    ], 200);
+}
+
+public function history_detail(Request $request, $token = null)
+{
+    return response()->json([
+        'success' => true,
+        'message' => 'History detail success',
+        'data' => [],
+    ], 200);
+}
 }
